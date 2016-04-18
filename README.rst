e844f851018c34e551c31b4e563bc2069a47f0af
Volodymyr Vitvitskyi <contact.volodymyr@gmail.com>
2016-04-18 22:43:32 +0100

    Syntax highlight

---
+++ README.rst
@@ -0,0 +1,8 @@
+Simple major mode for the cram files. Major adds syntax highlighting.
+
+
+References
+----------
+
+   * `How to Write a Emacs Major Mode for Syntax Coloring
+   <http://ergoemacs.org/emacs/elisp_syntax_coloring.html>`_ 
---
+++ cram-mode.el
@@ -0,0 +1,43 @@
+;;; cram-mode.el --- major mode for editing cram.
+
+;;; Copyright (C) 2016, by Volodymyr Vitvitskyi
+
+;; Author: Volodymyr Vitvitskyi ( contact.volodymyr@gmail.com )
+;; Version: 2016.5.1
+;; Created: 18 Apr 2016
+;; Keywords: languages
+;; Homepage: https://github.com/signalpillar/cram-mode
+
+;; This file is not part of GNU Emacs.
+
+;;; License:
+
+;; You can redistribute this program and/or modify it under the terms of the GNU General Public License version 2.
+
+;;; Commentary:
+
+;; Syntax highlight for cram file.
+;; https://pypi.python.org/pypi/cram
+
+;; full doc on how to use here
+
+
+;;; Code:
+
+(setq cram-highlights
+      '(("  [\\$>].*$" . font-lock-string-face)
+        ("  .*$" . font-lock-defaults)
+        (".*" . font-lock-comment-face)))
+
+(setq mode-name "cram")
+
+(define-derived-mode cram-mode fundamental-mode
+  (setq font-lock-defaults '(cram-highlights))
+  (setq mode-name "cram"))
+
+;; Local Variables:
+;; coding: utf-8
+;; End:
+
+(provide 'cram-mode)
+;;; cram-mode.el ends here
